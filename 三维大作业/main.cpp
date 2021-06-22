418446d0a3bb7c87f4b385962be503e4589d1d40
Feng <1315185856@qq.com>
2021-06-22 23:12:37 +0800

    基本完成木桩的各项功能

--- 三维大作业/main.cpp
+++ 三维大作业/main.cpp
@@ -5,7 +5,12 @@
 using namespace std;
 
 /*************************************************全局变量*************************************************/
-float landWidth = 50;
+float landWidth = 50;	//土地宽度
+float cameraX = 0;		//相机旋转角度
+float woodX = 0, woodY = 0, woodZ = 0, woodDegree = 0, woodScaleX = 1, woodScaleY = 1, woodScaleZ = 1;	//木桩的初始位置，旋转角度和放缩比例
+float disWoodX = 0, disWoodY = 0, disWoodZ = 0;		//木桩的移动位置
+bool mouseLeftDown=false;		//鼠标左键是否按下
+int mouseX = 0, mouseY = 0;		//鼠标位置
 /**********************************************************************************************************/
 
 /*************************************************函数声明*************************************************/
@@ -24,7 +29,7 @@ void Initialize() {
 	//光照设置
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(0, 20, 100, 0, 10, 0, 0, 1, 0);	//将摄像机放置于(0,50,100)位置，看向(0,0,0)
+	gluLookAt(0, 20, 100, 0, 0, 0, 0, 1, 0);	//将摄像机放置于(0,50,100)位置，看向(0,0,0)
 	glMatrixMode(GL_PROJECTION); //subsequent matrix operations are on the PROJECTION matrix
 	glLoadIdentity();// load the identity matrix for the projection matrix first
 	gluPerspective(45.0f, 960.0f / 720.0f, 0.1f, 200.0f); // (angle of view, aspect ratio, near, far clipping planes)
@@ -39,6 +44,11 @@ void Display() {
 	Wood();
 	Light();
 	Snow();
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(sin(cameraX/180*3.1415)*100, 20, cos(cameraX / 180 * 3.1415) * 100, 0, 0, 0, 0, 1, 0);
+	//cameraX += 0.1;
+	woodDegree += 0.1;
 	glFlush();//force execution of GL commands
 	glutPostRedisplay();
 }
@@ -51,16 +61,22 @@ void NormalKeys(unsigned char normalkey, int x, int y) {
 	}
 }
 void SpecialKeys(GLint specialkey, int x, int y) {
+	int mod;
 	switch (specialkey) {
 	case GLUT_KEY_UP:
+		woodScaleY = woodScaleY+0.1;
 		break;
 	case GLUT_KEY_DOWN:
+		woodScaleY -= 0.1;
 		break;
 	case GLUT_KEY_LEFT:
+		woodScaleX += 0.1;
 		break;
 	case GLUT_KEY_RIGHT:
+		woodScaleX -= 0.1;
 		break;
 	}
+	//cout << specialkey<<" ";
 	return;
 }
 void Reshape(int w, int h) {
@@ -70,6 +86,38 @@ void Reshape(int w, int h) {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(45, (GLfloat)w / (GLfloat)h, 0.1f, 1000.0f);
+}
+
+void Mouse(int button,int state,int x,int y) {
+	mouseX = x;
+	mouseY = y;
+	if (button == GLUT_LEFT_BUTTON) {
+		if (state == GLUT_DOWN) {
+			mouseLeftDown = true;
+		}
+		else if (state == GLUT_UP) {
+			mouseLeftDown = false;
+		}
+	}
+	else if (button == GLUT_RIGHT_BUTTON) {
+		//木桩复原
+		woodDegree = 0;
+		disWoodX = 0;
+		disWoodY = 0;
+		disWoodZ = 0;
+		woodScaleX = 1;
+		woodScaleY = 1;
+		woodScaleZ = 1;
+	}
+}
+
+void MouseMotion(int x, int y) {
+	if (mouseLeftDown) {
+		disWoodX += (float)(x - mouseX)/10;
+		disWoodZ += (float)(y - mouseY)/10;
+		mouseX = x;
+		mouseY = y;
+	}
 }
 
 int main(int iArgc, char** cppArgv) {
@@ -84,16 +132,57 @@ int main(int iArgc, char** cppArgv) {
 	glutKeyboardFunc(NormalKeys);	//普通按键回调函数
 	glutSpecialFunc(SpecialKeys);	//特殊按键回调函数
 	glutReshapeFunc(Reshape);		//屏幕大小改变回调函数
+	glutMouseFunc(Mouse);			//检测鼠标单击
+	glutMotionFunc(MouseMotion);		//检测鼠标拖动
 	glutMainLoop();//put the program in an infinite event loop
 	return 0;
 }
 
 void Screen() {
 	Screen_Land();
-	cout << "";
 }
 
-void Wood(){}
+void Wood(){
+	glPushMatrix();
+	glTranslatef(woodX + disWoodX, woodY + disWoodY, woodZ + disWoodZ);
+	glRotatef(woodDegree, 0, 1, 0);
+	glScalef(woodScaleX, woodScaleY, woodScaleZ);
+	glBegin(GL_QUADS);
+	glColor3f(0 / 256.0, 0 / 256.0, 0 / 256.0);
+	//底面
+	glVertex3f(-2.0, 0.0, -2.0);
+	glVertex3f(2.0, 0.0, -2.0);
+	glVertex3f(2.0, 0.0, 2.0);
+	glVertex3f(-2.0, 0.0, 2.0);
+	//顶面
+	glVertex3f(-2.0, 5.0, -2.0);
+	glVertex3f(2.0, 5.0, -2.0);
+	glVertex3f(2.0, 5.0, 2.0);
+	glVertex3f(-2.0, 5.0, 2.0);
+	//前
+	glColor3f(256.0 / 256.0, 0 / 256.0, 0 / 256.0);
+	glVertex3f(-2.0, 0.0, 2.0);
+	glVertex3f(2.0, 0.0, 2.0);
+	glVertex3f(2.0, 5.0, 2.0);
+	glVertex3f(-2.0, 5.0, 2.0);
+	//后
+	glVertex3f(-2.0, 0.0, -2.0);
+	glVertex3f(2.0, 0.0, -2.0);
+	glVertex3f(2.0, 5.0, -2.0);
+	glVertex3f(-2.0, 5.0, -2.0);
+	//左
+	glVertex3f(-2.0, 0.0, -2.0);
+	glVertex3f(-2.0, 0.0, 2.0);
+	glVertex3f(-2.0, 5.0, 2.0);
+	glVertex3f(-2.0, 5.0, -2.0);
+	//右
+	glVertex3f(2.0, 0.0, -2.0);
+	glVertex3f(2.0, 0.0, 2.0);
+	glVertex3f(2.0, 5.0, 2.0);
+	glVertex3f(2.0, 5.0, -2.0);
+	glEnd();
+	glPopMatrix();
+}
 void Light(){}
 void Snow(){}
 
